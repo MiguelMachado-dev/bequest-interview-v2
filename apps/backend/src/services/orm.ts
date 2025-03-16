1816e3d857f197500e6db9f5530d44f45242d4e7
Miguel Machado <hello@miguelmachado.dev>
2025-03-16 16:03:23 -0300

    feat: simplify ORM configuration by enabling synchronization and removing production-specific settings

--- apps/backend/src/services/orm.ts
+++ apps/backend/src/services/orm.ts
@@ -1,13 +1,9 @@
 import { DataSourceOptions } from 'typeorm';
 import { Document } from '../documents/entities/document.entity';
-import { join } from 'path';
 
 export const config: DataSourceOptions = {
   type: 'sqlite',
   database: '.database/sql',
-  synchronize: process.env.NODE_ENV !== 'production',
+  synchronize: true,
   entities: [Document],
-  migrations: [join(__dirname, '../migrations/**/*{.ts,.js}')],
-  migrationsRun: process.env.NODE_ENV === 'production',
-  logging: process.env.NODE_ENV !== 'production',
 };
