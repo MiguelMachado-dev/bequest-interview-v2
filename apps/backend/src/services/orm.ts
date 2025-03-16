31df3b42fd6a9de48a3448cca2c78dca84b52593
Miguel Machado <hello@miguelmachado.dev>
2025-03-16 14:43:18 -0300

    feat: update ORM configuration for production environment with dynamic settings for synchronization, migrations, and logging

--- apps/backend/src/services/orm.ts
+++ apps/backend/src/services/orm.ts
@@ -1,9 +1,13 @@
 import { DataSourceOptions } from 'typeorm';
 import { Document } from '../documents/entities/document.entity';
+import { join } from 'path';
 
 export const config: DataSourceOptions = {
   type: 'sqlite',
   database: '.database/sql',
-  synchronize: true,
+  synchronize: process.env.NODE_ENV !== 'production',
   entities: [Document],
+  migrations: [join(__dirname, '../migrations/**/*{.ts,.js}')],
+  migrationsRun: process.env.NODE_ENV === 'production',
+  logging: process.env.NODE_ENV !== 'production',
 };
